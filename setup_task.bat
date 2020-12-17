1c8453b439c52eab013faff257142afb6e7bd58f
Richard Graver <RchGrav@users.noreply.github.com>
2020-12-17 14:07:12 -0500

    Update setup_task.bat

--- setup_task.bat
+++ setup_task.bat
@@ -3,20 +3,22 @@
 where node.exe >nul 2>&1 || goto NodeMissing
 
 set taskname=get-my-ps5
+set starttime=14:00
+set duration=4:00
+set schedule=DAILY
 
 SCHTASKS /Query /TN %taskname% >NUL 2>&1
 
 if %errorlevel% NEQ 0 (
-    SCHTASKS /Create /RU %username% /IT /TN %taskname% /SC minute /MO 5 /TR "%~dp0%taskname%.cmd" /F || goto UACPrompt
+    SCHTASKS /Create /RU %username% /IT /TN %taskname% /SC %schedule% /ST %starttime% /DU %duration% /TR "%~dp0%taskname%.cmd" /F || goto UACPrompt
 	echo @cd /d "%~dp0" > "%~dp0%taskname%.cmd"
 	echo @if not exist node_modules call npm install >> "%~dp0%taskname%.cmd"
 	echo @call npm run watch >> "%~dp0%taskname%.cmd"
 	echo.
-	echo Starting %taskname% task now...
+	echo Created %taskname% task to run at %starttime% for %duration% hours...
 	ping 127.0.0.1 -n 6 > nul
-	SCHTASKS /Run /TN %taskname%
 ) else (
-    SCHTASKS /End /TN %taskname% || goto UACPrompt
+    echo Removing %taskname% task...
     SCHTASKS /Delete /TN %taskname% /F
 	del "%~dp0%taskname%.cmd"
 	ping 127.0.0.1 -n 3 > nul
